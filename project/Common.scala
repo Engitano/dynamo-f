1947ee9cb04fed1c085c52e3253af1b83919a354
Mark Tranter <mark.a.tranter@gmail.com>
2019-10-17 10:42:32 +1100

    Dont support 212

--- project/Common.scala
+++ project/Common.scala
@@ -6,8 +6,8 @@ object Common {
 
   val kindProjector = "org.typelevel" % "kind-projector" % "0.10.3" cross CrossVersion.binary
   val scala213 = "2.13.0"
-  val scala212 = "2.12.8"
-  val supportedScalaVersions = List(scala213, scala212)
+  // val scala212 = "2.12.8"
+  val supportedScalaVersions = List(scala213)
 
   def apply() = Seq(
     scalaVersion := "2.13.0",
