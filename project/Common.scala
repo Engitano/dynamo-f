b03cf96f61cc4b1ccceb469fb5eb9383cab42fba
Mark Tranter <mark.a.tranter@gmail.com>
2020-09-04 11:21:01 +1000

    Bump to scala 2.13.3

--- project/Common.scala
+++ project/Common.scala
@@ -4,20 +4,22 @@ import sbt.URL
 
 object Common {
 
-  val kindProjector = "org.typelevel" % "kind-projector" % "0.10.3" cross CrossVersion.binary
-  val scala213 = "2.13.0"
-  val scala212 = "2.12.10"
+  val scala213               = "2.13.3"
+  val scala212               = "2.12.10"
   val supportedScalaVersions = List(scala213, scala212)
 
   def apply() = Seq(
+    autoCompilerPlugins := true,
     scalaVersion := scala213,
     organization := "com.engitano",
     organizationName := "Engitano",
-    startYear := Some(2019),    
+    startYear := Some(2019),
     licenses += ("MIT", new URL("http://opensource.org/licenses/MIT")),
     crossScalaVersions := supportedScalaVersions,
     resolvers ++= Dependencies.resolvers(),
-    addCompilerPlugin(kindProjector),
-    libraryDependencies += Dependencies.collectionCompat,
+    libraryDependencies ++= Seq(
+      Dependencies.collectionCompat,
+      compilerPlugin("org.typelevel" % "kind-projector" % "0.11.0" cross CrossVersion.full)
+    )
   )
 }
