a0aaf508ac541be34591d44c51ff44d81223bbb3
Mark Tranter <mark.a.tranter@gmail.com>
2020-10-08 15:57:54 +1100

    Fix seq issue

--- core/src/main/scala/com/engitano/dynamof/JavaRequests.scala
+++ core/src/main/scala/com/engitano/dynamof/JavaRequests.scala
@@ -149,7 +149,7 @@ private object JavaRequests {
   def toAttributeDefinitions(pk: PrimaryKey*) = pk.flatMap {
     case SimpleKey(attr)      => Seq(toAttributeDefinition(attr))
     case CompositeKey(hk, rk) => Seq(toAttributeDefinition(hk), toAttributeDefinition(rk))
-  }.groupBy(_.attributeName()).map(_._2.head)
+  }.groupBy(_.attributeName()).map(_._2.head).toSeq
 
   def buildKeySchemaElement(name: String, keyType: KeyType) = KeySchemaElement.builder().keyType(keyType).attributeName(name).build
   def toKeySchemaDefinitions(pk: PrimaryKey) = pk match {
